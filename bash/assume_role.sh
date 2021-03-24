f4235c9d71bc0e619085a4fcb3e8e82c5e97cb66
Timothy Goetsch <timothy.goetsch@uspsector.com>
2021-03-24 00:09:23 -0600

    Correct typos

--- bash/assume_role.sh
+++ bash/assume_role.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -e
 current_identity=$(aws sts get-caller-identity | jq -r '.Arn' | sed -r 's/.*:([^:]+)/\1/;s/.*\/([^\/]+)/\1/')
 new_session=$(aws sts assume-role --role-arn $1 --role-session-name $current_identity)
 
 export AWS_ACCESS_KEY_ID=$(echo $new_session | jq -r '.Credentials.AccessKeyId')
-export AWS__SECRET_ACCESS_KEY=$(echo $new_session | jq -r '.Credentials.SecretAccessKey')
-export AWS_SESSION_RTOKEN=$(echo $new_session | jq -r '.Credentials.SessionToken')
+export AWS_SECRET_ACCESS_KEY=$(echo $new_session | jq -r '.Credentials.SecretAccessKey')
+export AWS_SESSION_TOKEN=$(echo $new_session | jq -r '.Credentials.SessionToken')
 unset current_identity
 unset new_session
---
+++ bash/unset_aws_auth_vars.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_ACCESS_KEY_ID
+unset AWS_SESSION_TOKEN
